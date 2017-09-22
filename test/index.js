2ff59a62cb2ba8ba26ef4b559c5842345c77b670
Saleh <saleh@grasppe.com>
2017-09-22 19:25:38 -0400

    Fix: corrected typo in test/index.js

--- test/index.js
+++ test/index.js
@@ -5,6 +5,6 @@ const canUsePolyestrModules = tryThis(() => !!require.resolve('polyestr-utils/li
 if (canUsePolyestrModules) {
     const banner = require('polyestr-utils/lib/bootstrap-banner')(require(`../package.json`));
     console.info('\n  √ Everything looks good.\n')
-} else if (shouldUseTSNode) {
+} else {
     throw Error(`Cannot run modules tests on the package without building it first.\n\tTry running "yarn build" in "${__dirname.replace(/.test.?$/)}".`);
 }
