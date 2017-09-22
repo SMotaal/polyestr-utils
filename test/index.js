dac375afe3e748c89f944c5b38aba1ff71455cc8
Saleh <saleh@grasppe.com>
2017-09-22 19:30:45 -0400

    Fix: change to relative paths in test/index.js

--- test/index.js
+++ test/index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node --
 const tryThis = (ƒ, fallback) => { try { return ƒ(); } catch (exception) { return typeof fallback === 'function' ? fallback(exception) : typeof fallback === 'object' ? (fallback.exception = exception) : fallback; }; };
-const canUsePolyestrModules = tryThis(() => !!require.resolve('polyestr-utils/lib/bootstrap-banner'), false);
+const canUsePolyestrModules = tryThis(() => !!require.resolve('../lib/bootstrap-banner'), false);
 
 if (canUsePolyestrModules) {
-    const banner = require('polyestr-utils/lib/bootstrap-banner')(require(`../package.json`));
+    const banner = require('../lib/bootstrap-banner')(require(`../package.json`));
     console.info('\n  √ Everything looks good.\n')
 } else {
     throw Error(`Cannot run modules tests on the package without building it first.\n\tTry running "yarn build" in "${__dirname.replace(/.test.?$/)}".`);
